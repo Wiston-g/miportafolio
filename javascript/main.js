28967d4b686017030176a376f3c9d20db8f35032
wiston  gonzales <53524033+Wiston-g@users.noreply.github.com>
2022-02-21 23:19:29 -0500

    quite boostrap y agrage materialize

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,26 @@
+//--------funciones materialize
+document.addEventListener('DOMContentLoaded', function() {
+    M.AutoInit();
+});    
+
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('.sidenav');
+var instances = M.Sidenav.init(elems, options);
+});
+  
+document.addEventListener('DOMContentLoaded', () =>{
+    const elementCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementCarousel, {
+        duration: 1000,
+        shif: 5,
+        padding: 5,
+        indicators: true,
+
+    });
+})
+//--------fin funciones materialize
+
+//--------funcion que escribe
 
 let parrafo = document.getElementById('animacion');
 let servicio = [' Landingpage ', ' Blog ', ' Website '];
@@ -24,7 +47,11 @@ const maquinaEscribir=(text='', tiempo = 20, etiqueta='')=>{
     }, tiempo)
 }
 
-maquinaEscribir(servicio[0], 200, parrafo)
+maquinaEscribir(servicio[0], 200, parrafo);
+//--------fin funcion que escribe
+
+
+
 
 
 
